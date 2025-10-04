2b4125a19b21c7de809e98f5726e17b75ad55b76
inkyssj <damus203@gmail.com>
2025-10-04 12:53:22 -0300

    Update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -1,6 +1,12 @@
+const { removeAccents } = require('../lib/functions')
+const { client, sms } = require('../lib/simple')
+
 module.exports = async(sock, m, plugins) => {
   try {
-    
+    sock = await client(sock)
+    m = await sms(sock, m)
+
+    console.log(m)
   } catch(e) {
     console.log('Error en messages.upsert: ', e)
   }
