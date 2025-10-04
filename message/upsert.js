26daac3895d21c6cbcb74b227d24f20e3454682e
inkyssj <damus203@gmail.com>
2025-10-04 13:46:04 -0300

    Update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -5,7 +5,8 @@ module.exports = async(sock, m, plugins) => {
   try {
     sock = await client(sock)
     m = await sms(sock, m)
-    console.log(m.type)
+    
+    console.log(m)
   } catch(e) {
     console.log('Error en messages.upsert: ', e)
   }
