984729fe2ba1636859e0a3d61971e6c899e94fb8
inkyssj <damus203@gmail.com>
2025-10-04 13:40:28 -0300

    Update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -4,8 +4,8 @@ const { client, sms } = require('../lib/simple')
 module.exports = async(sock, m, plugins) => {
   try {
     sock = await client(sock)
-    console.log(m)
     m = await sms(sock, m)
+    console.log(m.type)
   } catch(e) {
     console.log('Error en messages.upsert: ', e)
   }
