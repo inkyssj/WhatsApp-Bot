e6e12923060c6be84aa1b6a1de7d4699b66c4e46
inkyssj <damus203@gmail.com>
2025-10-04 14:08:33 -0300

    Update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -4,10 +4,8 @@ const { sms } = require('../lib/simple')
 module.exports = async(sock, m, plugins) => {
   try {
     m = await sms(m)
-
-    const base64 = Buffer.from(m.message.messageContextInfo.messageSecret).toString('base64');
-    console.log('Base64:', base64)
-    console.log(m.message.messageContextInfo)
+    
+    console.log(m.message)
   } catch(e) {
     console.log('Error en messages.upsert: ', e)
   }
